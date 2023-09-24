0eeef4b4bc777858b0aef4c0e913c4e83d494a5a
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-09-24 05:12:05 -0700

    rotate the stack to the top.

---
+++ rotl.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+#include <stdio.h>
+/**
+ * rotl -  rotates the stack to the top.
+ * @stack: A pointer to the pointer to the top of the stack.
+ * @line_number: The line number in the Monty bytecode file where the push
+ */
+void rotl(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node;
+	int temp;
+
+	node = *stack;
+	temp = node->n;
+	while (node->next != NULL)
+	{
+		node->n = node->next->n;
+		node = node->next;
+	}
+	node->n = temp;
+	(void)line_number;
+}
